07e5319a60b6fef3df5058dfabae0f001ea6f2d7
Sukhesh-Garg07 <sgwas77@gmail.com>
2026-05-17 11:42:31 +0530

    Update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBIIaP75F378Uw4eLBFfiEjuH2gc5OiQNY",
+  authDomain: "atomquest-hackathon-project.firebaseapp.com",
+  projectId: "atomquest-hackathon-project",
+  storageBucket: "atomquest-hackathon-project.firebasestorage.app",
+  messagingSenderId: "1031921889015",
+  appId: "1:1031921889015:web:1c03d3a785c9edb9e82df2"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
